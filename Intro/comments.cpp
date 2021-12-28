dc1a410e50af30dde7eae0266e474f223e0a392f
Prashik Raut <prashik856@gmail.com>
2021-12-29 00:23:24 +0530

    Completed intro

---
+++ Intro/comments.cpp
@@ -0,0 +1,20 @@
+// explanatory statements, compiler will not run this statement.
+// -> THis is how we start a comment.
+#include<iostream>
+using namespace std; // convention
+
+int main(){
+    // Printing hello
+    // cout << "Hello" << endl;
+    cout << "Hello" << endl;
+
+    /*
+    This is multiline comment
+    Also called as Block comment
+    klasjdflaksj
+    klas;lkdf;lkasjlwejr
+    lkjsal;jfls 
+    compiler doesn't care
+    */
+    return 0;
+}
---
+++ Intro/intro.cpp
@@ -0,0 +1,34 @@
+// iotstream is a header.
+// string, math, and other headers.
+#include<iostream>
+// #include - used to include header files.
+// iostream -> cout (screen print), cin (input from keyboard), main() (driving function), and much 
+using namespace std; // convention
+
+int main(){
+    int a;
+    // cout << "Hello world!"; // cout is used with '<<'
+    // cout << "This" << "is" << "awesome"; // multipel '<<' operators
+    // ; is used to terminate statement
+
+    // cout << "Hello world" << endl;
+    // cout << "I love Programming" << endl;
+    // endl -> used to add a new line.
+
+    // "double quotes" -> this is called a string
+    // '0-9, a-z, A-Z, !@#@!#$' -> character (char)
+    // "\n" -> new line, '\n' is also a character
+    // "\t" -> new tab
+    // cout << "Hello World!\n";
+    // cout << "Hellow World\t";
+
+    // backslash -> \, "
+    // cout << "Hello\" << endl;
+    cout << "'hello'" << endl;
+    return 0;
+}
+
+/*
+Hi Prashik,
+How are you?
+*/
